0e5431afbe7016d46741ec202eac679d023cf875
christian <christiankamdem2@gmail.com>
2021-03-31 08:02:18 -0700

    about us

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Public from '../../layouts/Public';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 function Services () {
   return (
     <div>
-      Services
+      	<div className="aboutus_page_section_image">
+		    <Jumbotron className="services_page_section_image_jombotron">
+		        <h1>Qu'est ce que</h1>
+		        <h2>Shakazz?</h2>
+		        <p>
+		          Généralement, on utilise un texte en faux latin.
+		        </p>
+		    </Jumbotron>
+	    </div>
     </div>
   )
 }
